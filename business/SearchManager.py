cfd009d66a3fb2c003662b262243d2516cb1707c
prenyx <stheerynx@gmail.com>
2024-05-06 10:33:43 +0200

    - deleted Hotel.py, Room.py - SearchManager.py; updated

--- business/SearchManager.py
+++ business/SearchManager.py
@@ -1,49 +1,87 @@
-# include all search functions here
-# accept search criteria, search by various criteria
-
 import os
-from sqlalchemy import select, func
-from business.BaseManager import BaseManager
+from pathlib import Path
+
+from data_access.data_base import init_db
+from sqlalchemy import select, func, create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+
 from data_models.models import *
 
 
-class HotelSearchCriteria:
-    """A class for setting up searching criteria for Hotels"""
+class SearchManager(object):
+    def __init__(self) -> None:
+        self.__load_db()
 
-    def __init__(self, hotel_name=None, hotel_location=None, hotel_rating=None, price_range=None, amenities=None,
-                 dates=None, room_types=None, star_category=None):
-        self.criteria = {k: v for k, v in
-                         {"hotel_name": hotel_name,
-                          "hotel_location": hotel_location,
-                          "hotel_rating": hotel_rating,
-                          "price_range": price_range,
-                          "amenities": amenities,
-                          "dates": dates,
-                          "room_types": room_types,
-                          "star_category": star_category}.items() if
-                         v is not None}
+    def __load_db(self):
+        # Ensure the environment Variable is set
+        if not os.environ.get("DB_FILE"):
+            raise ValueError("You have to define the environment variable 'DB_FILE'")
+        self.__db_filepath = Path(os.environ.get("DB_FILE"))
 
-    def get_criteria(self):
-        return self.criteria
+        # Ensure the db file exists, if not initialize a new db with or without example data
+        # You have to delete the db file, if you need a new fresh db.
+        if not self.__db_filepath.is_file():
+            init_db(str(self.__db_filepath), generate_example_data=True)
+
+        self._engine = create_engine(f'sqlite:///{self.__db_filepath}')
+        self._session = scoped_session(sessionmaker(bind=self._engine))
+
+    def get_hotels(self, name: str = "", city: str = ""):
+        query = select(Hotel)
+        if name != "":
+            query = query.where(Hotel.name.like(f'%{name}%'))
+        if city != "":
+            query = query.join(Address).where(Address.city.like(f"%{city}%"))
+
+        print(query)
+        return self._session.execute(query).scalars().all()
+
+    def get_hotels_by_name(self, name: str = ""):
+        query = select(Hotel)
+        if name != "":
+            query = query.where(Hotel.name.like(f'%{name}'))
+        print(query)
+        return self._session.execute(query).scalars().all()
+
+    def get_hotels_by_city(self, city: str = ""):
+        query = select(Hotel)
+        if city != "":
+            query = query.join(Address).where(Address.city.like(f"%{city}%"))
+        print(query)
+        return self._session.execute(query).scalars().all()
 
 
-if __name__ == "__main__":
-    search_criteria1 = HotelSearchCriteria(hotel_name="Paradise Inn",
-                                           hotel_location="New York",
-                                           hotel_rating=4,
-                                           price_range="$100-$200",
-                                           amenities="Free WiFi",
-                                           dates="2023-01-01 to 2023-01-31",
-                                           room_types=["Deluxe", "Suite", "Standard"],
-                                           star_category=5)
+def show(hotels):
+    for hotel in hotels:
+        print(hotel)
 
-    print(search_criteria1.get_criteria())
 
-    # search_criteria2 = HotelSearchCriteria(hotel_location="New York",
-    #                                       hotel_rating=4,
-    #                                       price_range="$100-$200",
-    #                                       amenities="Free WiFi",
-    #                                       dates="2023-01-01 to 2023-01-31",
-    #                                       room_types=["Deluxe", "Suite", "Standard"],
-    #                                       star_category=5)
-    # print(search_criteria2.get_criteria())
+if __name__ == '__main__':
+    # This is only for testing without Application
+
+    # You should set the environment variable in the run configuration.
+    # However, we can set it here in python as well.
+    # Because we are executing this file in the folder ./business/
+    # we need to relatively navigate first one folder up and therefore,
+    # use ../data in the path instead of ./data
+    # if the environment variable is not set, set it to a default
+    if not os.environ.get("DB_FILE"):
+        os.environ["DB_FILE"] = input("Enter relative Path to db file: ")
+        while not Path(os.environ.get("DB_FILE")).parent.is_dir():
+            os.environ["DB_FILE"] = input("Enter relative Path to db file: ")
+    search_manager = SearchManager()
+    all_hotels = search_manager.get_hotels()
+    show(all_hotels)
+    all_hotels = search_manager.get_hotels()
+    input("Press Enter to continue...")
+
+    city_in = input('City: ')
+    hotels_by_city = search_manager.get_hotels_by_city(city_in)
+    show(hotels_by_city)
+    input("Press Enter to continue...")
+
+    name_in = input('Name: ')
+    city_in = input('City: ')
+    found_hotels = search_manager.get_hotels(name_in, city_in)
+    show(found_hotels)
+    input("Press Enter to continue...")
