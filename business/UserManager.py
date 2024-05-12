2535f46ec830d69962f2d1e10e34ef0576e04d53
Manuel Pasamontes <manuel.pasamontes@students.fhnw.ch>
2024-05-12 10:55:48 +0200

    -removed hashing for simplicity. -adjusted Login method but not working yet... need more time

--- business/UserManager.py
+++ business/UserManager.py
@@ -1,14 +1,12 @@
 import os
 import hashlib
 from pathlib import Path
-from sqlalchemy import create_engine, select
+from sqlalchemy import create_engine, select, and_
 from sqlalchemy.orm import scoped_session, sessionmaker
 from data_access.data_base import init_db
 from data_models.models import *
 
-from data_access.our_db import init_our_db
-
-
+from data_access.data_base import init_db
 
 
 class UserManager(object):
@@ -18,49 +16,54 @@ class UserManager(object):
         self.__engine = create_engine(f'sqlite:///{database_path}', echo=False)
         self.__session = scoped_session(sessionmaker(bind=self.__engine))
 
-    #    def __load_db(self):
-    #        db_filepath = Path(os.environ.get("DB_FILE", "../data/database.db"))
-    #        if not db_filepath.is_file():
-    #            init_db(str(db_filepath), generate_example_data=False)
-    #        self._engine = create_engine(f'sqlite:///{db_filepath}')
-    #        self._session = scoped_session(sessionmaker(bind=self._engine))
-
-    def hash_password(self, password):
-        """Hash a password for storing."""
-        salt = os.urandom(16)
-        pwdhash = hashlib.pbkdf2_hmac('sha256', password.encode('utf-8'), salt, 100000)
-        return salt + pwdhash
+    # class UserManager(object):
+    #     def __init__(self) -> None:
+    #         self.__load_db()
+    #
+    #     def __load_db(self):
+    #         if not os.environ.get("DB_FILE"):
+    #             raise ValueError("You have to define the environment variable 'DB_FILE'")
+    #         self.__db_filepath = Path(os.environ.get("DB_FILE"))
+    #
+    #         if not self.__db_filepath.is_file():
+    #             init_db(str(self.__db_filepath), generate_example_data=True)
+    #
+    #         self._engine = create_engine(f'sqlite:///{self.__db_filepath}')
+    #         self._session = scoped_session(sessionmaker(bind=self._engine))
 
     def register(self, username, password, role_name):
         """Register a new user."""
         session = self.__session()
-        if session.query(User).filter_by(username=username).first():
+        if session.query(Login).filter_by(username=username).first():
             return False, "Username already exists."
         role = session.query(Role).filter_by(name=role_name).first()
         if not role:
             role = Role(name=role_name)
             session.add(role)
             session.commit()
-        hashed_password = self.hash_password(password)
-        new_user = User(username=username, password=hashed_password, role=role)
+        new_user = Login(username=username, password=password, role=role)
         session.add(new_user)
         session.commit()
         return True, "User registerd successfully."
 
-    def login(self, username, password):
-        """Login a user."""
-        session = self.__session()
-        user = session.query(User).filter_by(username=username).first()
-        if user and self.check_password(user.password, password):
-            return True, f"Login successful. Role: {user.role.name}"
-        return False, "Invalid username or password."
+    # def register_user(self, email: str, password: str):
+    #     new_user = Login(email=email, password=password)
+    #     self.__session.add(new_user)
+    #     self.__session.commit()
+    #     return new_user
 
-    def check_password(self, hashed_password, user_password):
-        """Verify a stored password against one provided by user."""
-        salt = hashed_password[:16]
-        stored_hash = hashed_password[16:]
-        pwdhash = hashlib.pbkdf2_hmac('sha256', user_password.encode('utf-8'), salt, 100000)
-        return pwdhash == stored_hash
+    def login(self, username: str, password: str):
+        # password hashing would be strongly advised - as discussed with lecturers and for simplicitys sake, we will refrain from implementing this unless we have enough time to do so.
+        # user = self.__session.execute(
+        #     select(Login).where(and_(Guest.email == email, Login.password == password))
+        # ).scalars().first()
+        # return user
+        user = self.__session.query(RegisteredGuest).join(Login).filter(Login.username == username,
+                                                                        Login.password == password).first()
+        if user:
+            role_info = f' Role: {user.login.role.name}, Access Level: {user.login.role.access_level}'
+            return f"User {username} logged in successfully.{role_info}"
+        return "Invalid username or password."
 
     def check_reg_user(self, username, password):
         query = select(RegisteredGuest).join(Login).where(Login.username == username)
@@ -83,12 +86,17 @@ class UserManager(object):
                 else:
                     print("Password or Username wrong")
 
+    def close_session(self):
+        self.__session.remove()
+
 
 if __name__ == "__main__":
-    filepath = Path("../data/our_db.db")
+    filepath = Path("../data/my_db.db")
 
     um = UserManager(filepath)
     um.check_reg_user("sabrina.schmidt@bluemail.ch", "SuperSecret")
 
-    #print(um.register("admin", "hallo123", "admin"))
-    #print(um.login("admin", "hallo123"))
+    um.login("sabrina.schmidt@bluemail", "SuperSecret")
+
+    # print(um.register("admin", "hallo123", "admin"))
+    # print(um.login("admin", "hallo123"))
