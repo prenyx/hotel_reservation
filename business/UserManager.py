6ab222c4281f4d1abb98b2fa299c4d3945dde044
Manuel Pasamontes <manuel.pasamontes@students.fhnw.ch>
2024-05-26 18:11:03 +0200

    all this only works on the baseline file of the models.py -> to discuss with supavadee if she still intends to keep the pending changes on that file.
    -added register_existing_user and create_new_login and the respective option points in the console base also with basic input handling and password checking.

--- business/UserManager.py
+++ business/UserManager.py
@@ -1,9 +1,9 @@
 import os
 import hashlib
 from pathlib import Path
+
 from sqlalchemy import create_engine, select, and_
 from sqlalchemy.orm import scoped_session, sessionmaker
-from data_access.data_base import init_db
 from data_models.models import *
 
 from data_access.data_base import init_db
@@ -16,86 +16,78 @@ class UserManager(object):
         self.__engine = create_engine(f'sqlite:///{database_path}', echo=False)
         self.__session = scoped_session(sessionmaker(bind=self.__engine))
 
-    # class UserManager(object):
-    #     def __init__(self) -> None:
-    #         self.__load_db()
-    #
-    #     def __load_db(self):
-    #         if not os.environ.get("DB_FILE"):
-    #             raise ValueError("You have to define the environment variable 'DB_FILE'")
-    #         self.__db_filepath = Path(os.environ.get("DB_FILE"))
-    #
-    #         if not self.__db_filepath.is_file():
-    #             init_db(str(self.__db_filepath), generate_example_data=True)
-    #
-    #         self._engine = create_engine(f'sqlite:///{self.__db_filepath}')
-    #         self._session = scoped_session(sessionmaker(bind=self._engine))
+    def get_session(self):
+        return self.__session
 
-    def register(self, username, password, role_name):
-        """Register a new user."""
-        session = self.__session()
-        if session.query(Login).filter_by(username=username).first():
-            return False, "Username already exists."
-        role = session.query(Role).filter_by(name=role_name).first()
-        if not role:
-            role = Role(name=role_name)
-            session.add(role)
-            session.commit()
-        new_user = Login(username=username, password=password, role=role)
-        session.add(new_user)
+    def create_new_login(self, email, password):
+        session = self.get_session()
+        existing_user = session.query(Login).filter_by(username=email).first()
+        if existing_user:
+            print("Email already exists. Please try again.")
+            session.close()
+            return False
+
+        new_login = Login(username=email, password=password, role_id=2)
+        session.add(new_login)
         session.commit()
-        return True, "User registerd successfully."
+        session.close()
+        print("New login created successfully.")
+        return True
 
-    def authenticate_user(self, username, password):
+    def register_existing_user(self, email, password):
+        session = self.get_session()
+        user = session.query(Login).filter_by(username=email, password=password).first()
+        if not user:
+            print("Invalid email or password.")
+            session.close()
+            return False
+
+        first_name = input("Enter first name: ")
+        last_name = input("Enter last name: ")
+        street = input("Enter street: ")
+        zip_code = input("Enter zip code: ")
+        city = input("Enter city: ")
+
+        print(f"Debug:" + street, zip_code, city)
+        address = Address(street=street, zip=zip_code, city=city)
+        session.add(address)
+        session.commit()
+
+        registered_guest = RegisteredGuest(
+            firstname=first_name,
+            lastname=last_name,
+            email=email,
+            address_id=address.id,
+            login_id=user.id
+        )
+
+        session.add(registered_guest)
+        session.commit()
+        session.close()
+
+        # address_id = address.id
+        #
+        # print(f"Debug:" + first_name, last_name, email, address_id)
+        # guest = Guest(firstname=first_name, lastname=last_name, email=email, address_id=address_id, type='registered')
+        # session.add(guest)
+        # session.commit()
+
+        # registered_guest = RegisteredGuest(login_id=user.id, id=guest.id)
+        # session.add(registered_guest)
+        # session.commit()
+        # session.close()
+        print("User registered successfully.")
+        return True
+
+    def authenticate_user(self, email, password):
         """Authenticate a user based on username and password."""
         # password hashing would be strongly advised - as discussed with lecturers and for simplicity's sake,
         # we will refrain from implementing this unless we have enough time to do so.
         # print("Authenticating user:", username, "with password:", password)  # debug what is getting passed into query
-        query = select(Login).where(Login.username == username, Login.password == password)
-        result = self.__session.execute(query).scalars().first()
-        #print(query) debug what the query is
-        if result:
-            print(f"Login successful for {result.username}")
-        else:
-            print("Password or Username wrong")
-
-    # def check_reg_user(self, username, password): ### first iteration of login,
-    # temporarily leaving this here incase i want to add more functionality
-    #     query = select(RegisteredGuest).join(Login).where(Login.username == username)
-    #     result = self.__session.execute(query).scalars().one_or_none()
-    #     print(result)
-    #     if result:
-    #         print("Registered User")
-    #         if result.login.password == password:
-    #             print("Login Successfull")
-    #         else:
-    #             print("Password or Username wrong")
-    #     else:
-    #         query_for_login = select(Login).where(Login.username == username)
-    #         result = self.__session.execute(query_for_login).scalars().one_or_none()
-    #         if result:
-    #             print("None Guest Login")
-    #             if result.password == password:
-    #                 print("Login Successfull")
-    #                 print(result.role.name)
-    #             else:
-    #                 print("Password or Username wrong")
-
-    def close_session(self):
-        self.__session.remove()
-
-
-if __name__ == "__main__":
-    filepath = Path("../data/my_db.db")
-
-    um = UserManager(filepath)
-    # um.check_reg_user("sabrina.schmidt@bluemail.ch", "SuperSecret")
-
-    # Example login test
-    um.authenticate_user("sabrina.schmidt@bluemail.ch", "SuperSecret")
-
-    # print(um.register("admin", "hallo123", "admin"))
-    # print(um.login("admin", "hallo123"))
-
-    um.register("manuel.pasamontes@bluewin.ch", "Secret",2)
-    for print()
+        session = self.get_session()
+        user = session.query(Login).filter_by(username=email, password=password).first()
+        if user:
+            print(f"Login successful for {user.email}")
+        if not user:
+            print("Invalid email or password.")
+            session.close()
--- console/console_base.py
+++ console/console_base.py
@@ -1,9 +1,23 @@
 import os
+from typing import Optional
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, scoped_session
+from business.UserManager import UserManager
+from data_access.data_base import init_db
+from data_models import models
+from data_models.models import Base, Guest, Login
+from pathlib import Path
 
 
 class Console(object):
-    def __init__(self):
-        pass
+
+    def __init__(self, database_path: Path):
+        if not database_path.is_file():
+            init_db(str(database_path), generate_example_data=True)
+        self.__engine = create_engine(f'sqlite:///{database_path}', echo=False)
+        self.__session = scoped_session(sessionmaker(bind=self.__engine))
+        self._database_path = database_path  # store the database_path as an instance variable
 
     def run(self):
         raise NotImplementedError("Implement this method")
@@ -38,8 +52,8 @@ class MenuOption(object):
 
 
 class Menu(Console):
-    def __init__(self, title, width=50):
-        super().__init__()
+    def __init__(self, title, database_path: Path, width=50):
+        super().__init__(database_path)
         self._title = title
         self._options = []
         self._width = width
@@ -78,10 +92,65 @@ class Menu(Console):
             choice = input("Enter Option: ")
         return int(choice)
 
-    def _navigate(self, choice: int):
-        raise NotImplementedError("Implement this method")
+    def _navigate(self, choice: int) -> Optional[Console]:
+        if choice == 1:
+            console = UserRegistrationConsole(self._database_path)
+            console.create_new_login()
+            return console
+        elif choice == 2:
+            console = UserRegistrationConsole(self._database_path)
+            console.register_existing_user()
+            return console
+        else:
+            return self
 
     def run(self) -> Console:
         self.clear()
         self._show()
         return self._navigate(self._make_choice())
+
+
+class UserRegistrationConsole(Console):
+    def __init__(self, database_path: Path):
+        super().__init__(database_path)
+        self._user_manager = UserManager(database_path)
+
+    def _navigate(self, choice: int) -> Optional[Console]:
+        if choice == 1:
+            self.create_new_login()
+        elif choice == 2:
+            self.register_existing_user()
+        else:
+            return None
+        return self
+
+    def create_new_login(self):
+        email = input("Enter email: ")
+        password = input("Enter password: ")
+        confirm_password = input("Confirm password: ")
+        if password != confirm_password:
+            print("Passwords do not match. Please try again.")
+            return
+        self._user_manager.create_new_login(email, password)
+
+    def register_existing_user(self):
+        email = input("Enter email: ")
+        password = input("Enter password: ")
+        self._user_manager.register_existing_user(email, password)
+
+    def run(self):
+        menu = Menu("Hotel Reservation System", self._database_path)
+        menu.add_option(MenuOption("Create New Login"))
+        menu.add_option(MenuOption("Register Existing User"))
+        return menu
+
+
+if __name__ == "__main__":
+    database_path = Path("../data/my_db.db")
+    app = UserRegistrationConsole(database_path)
+    # new_menu = app.run()
+    # new_menu.run()
+    # app.run()
+    next_console = app.run()
+    while next_console:
+        next_console = next_console.run()
