71a75e77ad502801551dc6233ce41ec3a5c04dc1
Benz <stheerynx@gmail.com>
2024-06-13 20:49:40 +0200

    - main.py Updated

--- main.py
+++ main.py
@@ -7,21 +7,26 @@ from console.console_base import *
 from ui.mainMenu import MainMenu
 from data_models.models import *
 
-DEFAULT_DB = "./data/hotel_reservation.db"
-TEST_DB = "./data/test_hotel_reservation.db"
+DB_DIR = "./data"
+DEFAULT_DB_FILE = "hotel_reservation.db"
+TEST_DB_FILE = "test_hotel_reservation.db"
+
+DEFAULT_DB = f"sqlite:///{DB_DIR}/{DEFAULT_DB_FILE}"
+TEST_DB = f"sqlite:///{DB_DIR}/{TEST_DB_FILE}"
 
 if __name__ == "__main__":
 
     # if the environment variable is not set, set it to default
     if not os.environ.get('DB_FILE'):
-        os.environ['DB_FILE'] = TEST_DB
+        os.environ['DB_FILE'] = TEST_DB_FILE
 
-    db_path = Path(os.environ.get('DB_FILE'))
+    db_path = Path(os.path.join(DB_DIR, os.environ.get('DB_FILE')))
 
     # creates the file if it does not exist
     db_path.touch(exist_ok=True)
 
-    __engine = create_engine(os.environ.get('DB_FILE'))
+    db_uri = f"sqlite:///{db_path.as_posix()}"
+    __engine = create_engine(db_path)
     metadata.create_all(bind=__engine)  # bind the engine to the metadata of the Base class
 
     # create the very first main menu
